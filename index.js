9153f8bce088a72fa83605cbf577d868c08f66e5
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-02-03 09:08:16 +0000

    We need a web server too, probably

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var ws = require('ws');
+var http = require('http');
 
-var server = new ws.Server({port: process.env.PORT || 5000});
-server.on('connection', function(conn) {
+var webserve = http.createServer(function(req, res) {
+	res.writeHead(200, {"Content-type": "text/plain"});
+	res.write("Future home of Turbo-Spork!");
+	res.end();
+}).listen(process.env.PORT || 5000);
+
+var sockserve = new ws.Server({server: webserve});
+sockserve.on('connection', function(conn) {
 	console.log(conn);
 	conn.on('message', function(message) {
 		console.log(message);
