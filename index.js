a7184ee27380abf159988fc0e504c08543bb22a0
Tony Brar <tonycbrar@gmail.com>
2016-06-01 21:51:09 -0600

    Fix creating and storing energy array

--- index.js
+++ index.js
@@ -402,21 +402,21 @@ var addbot = function(room) {
 
 var startGame = function(name) {
 	var nodes = [];
+	var energy = []
 	// create yo moms house with owner -3
 	nodes.push(createNode({owner: -3, x: GAMERULES.FIELD_SIZE / 2, y: GAMERULES.FIELD_SIZE / 2}, nodes));
-	games[name].energy = [];
 	for(var i = 0; i < games[name].users.length; i++) {
-		games[name].energy.push(GAMERULES.STARTING_ENERGY);
 		for(var x = 0; x < GAMERULES.NODES_PER_USER_AT_START; x++) {
 			nodes.push(createNode({
 				owner: i,
 			}, nodes));
 		}
+		games[name].data.energy.push(GAMERULES.STARTING_ENERGY);
 	}
 	for(var x = 0; x < GAMERULES.UNCLAIMED_NODES_AT_START; x++) {
 		nodes.push(createNode({}, nodes));
 	}
-	games[name].data = {nodes: nodes, removed: [], energy: []};
+	games[name].data = {nodes: nodes, removed: [], energy: energy};
 	broadcast("gameinfo:"+JSON.stringify(games[name].data), games[name]);
 	setTimeout(function() {
 		// make sure that game still exists before starting it
